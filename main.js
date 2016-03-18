6b6d1ee208d3bcd13bb2093f1857c5a736f39da7
Nianwen Song <nianwensong@gmail.com>
2016-03-18 14:59:30 -0700

    Initial commit of trying rest api

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 $(document).find("#main").append("<div>I HAVE LOADED SUCCESSFULLY</div>");
+$("#sendWI").click(function(eventData){
+    // Be replaced with the selected real action item
+    var sampleWorkItemData = [
+        {
+            "op": "add",
+            "path": "/fields/System.Title",
+            "value": "JavaScript implementation for Microsoft Account"
+        }
+
+    ];
+    VSS.require(["VSS/Service", "TFS/WorkItemTracking/RestClient"], function (VSS_Service, TFS_Wit_WebApi) {
+        var witClient = VSS_Service.getCollectionClient(TFS_Wit_WebApi.WorkItemTrackingHttpClient);
+         witClient.createWorkItem(sampleWorkItemData, "MyNewProject", "User Story").then(
+            function(workItems) {
+                console.log("Create Succeed!");
+        });
+    });
+});
