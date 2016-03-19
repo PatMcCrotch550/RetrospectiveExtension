37c66f85334441dd62e6539ab0ce310ae0bb4f6f
Pranav Karkhanis <prakark@microsoft.com>
2016-03-18 17:28:14 -0700

    Create workitem using rest api

--- dataStorage.js
+++ dataStorage.js
@@ -8,3 +8,21 @@ VSS.getService(VSS.ServiceIds.ExtensionData).then((dataService) => {
         });
 	});
 });
+$("#sendWI").click(function(eventData){
+    // Be replaced with the selected real action item
+    var sampleWorkItemData = [
+        {
+            "op": "add",
+            "path": "/fields/System.Title",
+            "value": "JavaScript implementation for Microsoft Account"
+        }
+
+    ];
+    VSS.require(["VSS/Service", "TFS/WorkItemTracking/RestClient"], function (VSS_Service, TFS_Wit_WebApi) {
+        var witClient = VSS_Service.getCollectionClient(TFS_Wit_WebApi.WorkItemTrackingHttpClient);
+        witClient.createWorkItem(sampleWorkItemData, "Retrospective", "User Story").then(
+            function(workItems) {
+                console.log("Create Succeeded!");
+        });
+    });
+}); 
